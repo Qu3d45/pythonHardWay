7d59bc9b996d3665e312ed3c532333717fec99b2
Lameira <lameira.manuel@gmail.com>
2019-05-28 14:48:25 +0100

    Add ex18

--- ex18.py
+++ ex18.py
@@ -1,2 +1,34 @@
 ﻿# Learn Pyhton The Hard Way ex18 - Names, Variables, Code, Functions
 # Manuel Lameira
+
+
+# this one is like your scripts with argv
+
+
+def print_two(*args):
+    arg1, arg2 = args
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+# ok, that *args is actually pointless, we can just do this
+
+
+def print_two_again(arg1, arg2):
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+# this just takes one argument
+
+
+def print_one(arg1):
+    print(f"arg1: {arg1}")
+
+# this one takes no arguments
+
+
+def print_none():
+    print("I got nothin'.")
+
+
+print_two("Manuel", "Lameira")
+print_two_again("Nicole", "Guilherme")
+print_one("First!")
+print_none()
