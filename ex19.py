e40df72a72669e7015c3d12fc0dfb3618b99400b
Lameira <lameira.manuel@gmail.com>
2019-05-28 17:03:17 +0100

    add ex19

---
+++ ex19.py
@@ -0,0 +1,24 @@
+﻿# Learn Pyhton The Hard Way ex19 - Functions and Variables
+# Manuel Lameira
+
+
+def cheese_and_crackers(chesse_cont, boxes_of_crackers):
+    print(f"You have {chesse_cont} cheese!")
+    print(f"You have {boxes_of_crackers} boxes of crackers!")
+    print("Man thats's enouth for a party!")
+    print("Get a blanket.\n")
+
+
+print("We can just give the function numbers directly:")
+cheese_and_crackers(20, 30)
+
+
+print("Or, we can use variables from our script:")
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+
+print("And we can combine the two, variables and math:")
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 100)
---
+++ ex19_drill.py
@@ -0,0 +1,30 @@
+﻿# Learn Pyhton The Hard Way ex19 - Functions and Variables
+# Manuel Lameira
+
+# DRILL
+# 1. Go back through the script and type a comment above each line explaining in English what it
+# does.
+# 2. Start at the bottom and read each line backward, saying all the important characters.
+# 3. Write at least one more function of your own design, and run it 10 different ways.
+
+
+def cheese_and_crackers(chesse_cont, boxes_of_crackers):
+    print(f"You have {chesse_cont} cheese!")
+    print(f"You have {boxes_of_crackers} boxes of crackers!")
+    print("Man thats's enouth for a party!")
+    print("Get a blanket.\n")
+
+
+print("We can just give the function numbers directly:")
+cheese_and_crackers(20, 30)
+
+
+print("Or, we can use variables from our script:")
+amount_of_cheese = 10
+amount_of_crackers = 50
+
+cheese_and_crackers(amount_of_cheese, amount_of_crackers)
+
+
+print("And we can combine the two, variables and math:")
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 100)
