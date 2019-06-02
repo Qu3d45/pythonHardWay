7eaff274fbb14c6cf1b97e5e44a86041a5876d58
Lameira <lameira.manuel@gmail.com>
2019-06-02 22:52:10 +0100

    add ex23 to 26

--- ex23_strings_bytes_char.py
+++ ex23_strings_bytes_char.py
@@ -1,2 +1,44 @@
 ﻿# Learn Pyhton The Hard Way ex23 - Strings, Bytes and Character Encodings
 # Manuel Lameira
+
+import sys
+script, input_encoding, error = sys.argv
+
+
+def main(language_file, encoding, errors):
+    # Debugging
+    # print(">>>> main", repr(language_file), encoding, errors)
+    line = language_file.readline()
+
+    if line:
+        # Debugging
+        # print(">> there's a line", repr(line))
+        print_line(line, encoding, errors)
+        # Debugging
+        # print(">> calling main again")
+        return main(language_file, encoding, errors)
+    # Debugging
+    # print("<<<< exit main")
+
+
+# Takes a line, the encoding (in this case UTF-8) and how to handel errors
+
+
+def print_line(line, encoding, errors):
+    # Debugging
+    # print(">>> print_line", repr(print_line), encoding, errors)
+    # Removes the withe space oe the new line symbole at the end of the line
+    next_lang = line.strip()
+    # Takes the line and transform (encodes) in to raw bytes b'...'
+    raw_bytes = next_lang.encode(encoding, errors=errors)
+    # Takes the raw bytes and decodes so we can se the original line value
+    cooked_string = raw_bytes.decode(encoding, errors=errors)
+
+    print(raw_bytes, "<===>", cooked_string)
+    # Debugging
+    # print("<<< exit print_line")
+
+
+languages = open("ex23_languages.txt", encoding="utf-8")
+
+main(languages, input_encoding, error)
