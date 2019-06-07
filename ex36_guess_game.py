346cb5f28ada2178b505a4860a20c1b800bd8658
Lameira <lameira.manuel@gmail.com>
2019-06-07 20:44:43 +0100

    add ex36 guess game

---
+++ ex36_guess_game.py
@@ -0,0 +1,24 @@
+﻿# Learn Pyhton The Hard Way ex36 - Number guessing Game
+# Manuel Lameira
+
+import random
+
+correct = False
+
+r = random.randint(1, 20)
+
+i = 0
+
+while correct == False:
+    n = input("Guess my number between 1 and 20: ")
+
+    i += 1
+    if int(n) == r:
+        correct = True
+    else:
+        if int(n) > r:
+            print("Sorry, my number is lower. Try again.")
+        else:
+            print("Sorry, my number is higher. Try again.")
+
+print(f"Well done. The correct answer was {r}. You got it in {i} tries.")
